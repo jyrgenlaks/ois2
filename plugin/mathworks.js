af06162f288fb0366f336b29d64ef84a351d5d37
Jürgen Laks <jyrgenlaks@gmail.com>
2020-09-13 19:01:00 +0300

    More efficient and effective mathworks popup dismisser

--- plugin/mathworks.js
+++ plugin/mathworks.js
@@ -1,12 +1,20 @@
-setTimeout(closePopup, 1000);
+setInterval(checkPopup, 500);
+
+function checkPopup(){
+    var d = document.getElementById("country-unselected").style.display;
+    if(d === "block"){
+        // The panel is visible
+        console.log("Popup opened");
+        closePopup();
+    }
+}
 
 function closePopup() {
     var spans = document.getElementsByTagName("span");
     var closeButton = null;
     for (var i = 0; i < spans.length; i++) {
         if (spans[i].innerText.includes("×")) {
-            closeButton = spans[i];            
-            console.log(spans[i]);
+            closeButton = spans[i];
         }
     }
     if(closeButton != null){
